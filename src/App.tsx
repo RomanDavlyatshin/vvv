6ec068b94d159e819d0124149f1e490a3868b310
RomanDavlyatshin <davlyatshinra@gmail.com>
2021-10-28 19:13:45 +0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,124 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+/*
+ * Copyright 2020 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import useOctokit from './github/hooks/use-octokit';
+import useUser from './github/hooks/use-user';
+import useLedgerContents from './github/hooks/use-ledger-contents';
+import Octokit from './github/octokit';
 import './App.css';
-import './third-party/git-connect/git-connect'
-import { Octokit } from '@octokit/core'
-
-const connection = (window as any).connection({
-  client_id: "684e39e0e69952196fd1", //required; your application `client_id` in Github
-  proxy: "http://versionion-test-git-proxy.herokuapp.com", //required; Base_URI to your git-proxy server
-  scope:'repo,user',
-  //scope: 'public_repo,gist', //optional, default: 'public_repo'; Github scopes like repo,gist,user,...
-  //expires: 7,  //optional, default: 7; the number of days after coockies expire
-
-  //this options are used and required only for `git-edit` module
-  // owner: 'github_username',  //application owner's github username
-  // reponame: 'github_reponame', //application's repository name
-});
+import { useState } from 'react';
 
 function App() {
-
+  const connection = useOctokit();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {!connection.isConnected()
-          ? <button onClick={() => githubLogin()}>Authenticate using GitHub</button>
-          : <button onClick={() => githubLogout()}>GitHub Logout</button>
-        }
-        {connection.isConnected() && <UserData/>}
+        {!connection.isConnected() ? (
+          <button onClick={() => connection.connect()}>Authenticate using GitHub</button>
+        ) : (
+          <button onClick={() => connection.disconnect()}>GitHub Logout</button>
+        )}
+        {connection.isConnected() && <UserData />}
       </header>
     </div>
   );
 }
-function b64_to_utf8( str ) {
-  return decodeURIComponent(escape(window.atob( str )));
-}
+
 function UserData() {
-  const [data, setData] = useState('');
-  useEffect(() => {
-    (async ()=> {
-      const auth = getCookie('github_access_token');
-      const octokit = new Octokit({ auth });
-
-      const owner = 'RomanDavlyatshin';
-      const repo = 'ledger';
-      const path = 'ledger.json';
-
-      const {
-        data,
-      } = await octokit.request(`GET /repos/${owner}/${repo}/contents/${path}`, {
-        raw: true
-      });
-
-      console.log(b64_to_utf8(data));
-      console.log(b64_to_utf8(data.contents));
-      debugger;
-      // const {
-      //   data: { login },
-      // } = await octokit.request("GET /user");
-      // setData(login);
-    })()
-  }, []);
+  const user = useUser();
+  const data = useLedgerContents();
+  const [isUpdating, setIsUpdating] = useState(false);
   return (
     <div>
-      <div>data</div>
-      { data ? <div> Hi there {data} !</div> : <div> waiting to receive user data... </div> }
-      {/* <div>{data.map((x, i) => <div>i</div>)}</div> */}
-      <div>end</div>
+      {user ? <div> Hi there {user.login} !</div> : <Spinner> waiting to receive user data... </Spinner>}
+      <h2>Ledger</h2>
+      <Ledger data={data} />
+      <button
+        onClick={async () => {
+          try {
+            setIsUpdating(true);
+            await updateLedger(data);
+          } catch (e) {
+            alert('failed to update ledger ' + (e as any)?.message || JSON.stringify(e));
+          } finally {
+            setIsUpdating(false);
+          }
+        }}
+      >
+        Update Ledger
+      </button>
+      {isUpdating && <Spinner>Updating ledger...</Spinner>}
     </div>
-  )
+  );
 }
 
-function getCookie(name: string) {
-  if (!document.cookie) return;
-  const cookies = document.cookie.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-    const [key, value] = cookies[i].split('=');
-    if (key === name) return value;
-  }
+async function updateLedger(data: any) {
+  const octokit = Octokit.getInstance();
+  if (!octokit) return;
+
+  const { content, sha } = data;
+  const { REACT_APP_LEDGER_REPO_OWNER: owner, REACT_APP_LEDGER_REPO_NAME: repo, REACT_APP_LEDGER_REPO_PATH: path } = process.env;
+
+  await octokit.request(`PUT /repos/${owner}/${repo}/contents/${path}`, {
+    message: 'foobar',
+    content: utf8_to_b64(
+      JSON.stringify({
+        ...content,
+        versions: [...content.versions, { name: 'foo', value: 'bar', date: Date.now() }],
+      }),
+    ),
+    sha,
+  });
 }
 
-function githubLogin() {  
-  if (connection.isConnected()) {
-    alert('already logged in');
-    return;
-  }
-  connection.connect();
+function Spinner(props: any) {
+  if (props.children) return <div>{props.children}</div>;
+  return <div>...</div>;
 }
 
-function githubLogout() {  
-  if (!connection.isConnected()) {
-    alert('already logged out');
-    return;
-  }
-  connection.disconnect();
+function Ledger(props: any) {
+  const data = props.data;
+  if (!data) return <Spinner />;
+  if (!data.content) return <Spinner>Ledger content is empty</Spinner>;
+  const { components, setups, versions, tests } = data.content;
+  return (
+    <div>
+      <Stats label="Components" data={components} />
+      <Stats label="Setups" data={setups} />
+      <Stats label="Versions" data={versions} />
+      <Stats label="Tests" data={tests} />
+    </div>
+  );
+}
+
+function Stats(props: any) {
+  return (
+    <div>
+      <h3>{props.label}</h3>
+      <div>{Array.isArray(props.data) ? props.data.map((x: any) => renderStat(x)) : '...'}</div>
+    </div>
+  );
+}
+
+function renderStat(x: any) {
+  return x.map((z: any) => (
+    <div>
+      {z.name}
+      {z.value && `: ${z.value}`}
+    </div>
+  ));
 }
 
 export default App;
